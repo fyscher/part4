a925c8ede0f2b0b1a652a2f1380c9f88562d3d10
Fyscher Kraal <fyscher.kraal@gmail.com>
2025-02-10 14:29:19 -0700

    complete to 4.1: create working npm project

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+})
+
+blogSchema.set('toJSON',
+{
+  transform: (document, returnedObj) =>
+  {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.set('strictQuery', false)
 
-app.use(cors())
-app.use(express.json())
+logger.info('Connecting to MongoDB: ', process.env.MONGODB_URI)
+mongoose.connect(mongoUrl)
+  .then(() =>
+  {
+    logger.info('MongoDB connection successful')
+  })
+  .catch((error) =>
+  {
+    logger.error('Connection Error: ', error.message)
+  })
+
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -26,17 +50,24 @@ app.get('/api/blogs', (request, response) => {
     })
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post('/api/blogs', (request, response) =>
+{
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
 
   blog
     .save()
     .then(result => {
+      logger.info('New Entry: ', blog)
       response.status(201).json(result)
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
 })
