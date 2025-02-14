86eb618689fea4da718841babc0929d611029926
fyscher <fyscher.kraal@gmail.com>
2025-02-14 15:58:07 -0700

    complete to 4.5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,13 +1,40 @@
 const dummy = blogs =>
 {
-   const reducer = (arr, blog) =>
-   {
-    return arr.push(blog)
-   }
     return 1
+}
+
+const totalLikes = blogs =>
+{
+    const reducer = (arr, blog) =>
+    {
+        return arr + blog.likes 
+    }
+    
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favouriteBlog = blogs =>
+{
+    const fave =  blogs.reduce((max, current) =>
+    {
+       return (current.likes > max.likes)
+       ? current
+       : max
+    })
+    return blogs === 0
+    ? 0
+    : {
+        title: fave.title,
+        author: fave.author,
+        likes: fave.likes
+    }
 }
 
 module.exports = 
 {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog
 }
