04f444268315c4c9e47885532666303e8a157847
fyscher <fyscher.kraal@gmail.com>
2025-02-16 22:37:28 -0700

    complete to 4.7*: passes mostBlogs and mostLikes

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,56 +2,6 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-  const blogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
-  ] 
 test('dummy returns one', () =>
 {
   const blogs = []
@@ -139,7 +89,7 @@ describe('total likes', () =>
 
 describe('favourite', () =>
 {
-  const blogs = [
+  const sampleBlogs = [
     {
       _id: "5a422a851b54a676234d17f7",
       title: "React patterns",
@@ -192,7 +142,7 @@ describe('favourite', () =>
 
   test('blog', () =>
   {
-    const result = listHelper.favouriteBlog(blogs)
+    const result = listHelper.favouriteBlog(sampleBlogs)
     assert.deepStrictEqual
     (
       result, 
@@ -202,5 +152,31 @@ describe('favourite', () =>
         likes: 12,
       }
     )
+  })
+  
+  test('author', () =>
+  {
+    const result = listHelper.favouriteAuthor(sampleBlogs)
+    assert.deepStrictEqual
+    {
+      result,
+      {
+        author: "Robert C. Martin",
+        blogs: 3
+      }
+    }
+  })
+  
+  test('most likes', () =>
+  {
+    const result = listHelper.mostLikes(sampleBlogs)
+    assert.deepStrictEqual
+    {
+      result,
+      {
+        author: "Edsger W. Dijkstra",
+        likes: 17
+      }
+    }
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash');
+
 const dummy = blogs =>
 {
     return 1
@@ -32,9 +34,65 @@ const favouriteBlog = blogs =>
     }
 }
 
+const favouriteAuthor = blogs =>
+{
+    const groups = lodash.groupBy(blogs, 'author')
+    const mapped = lodash.mapValues(groups, value => value.length)
+    const entries = Object.entries(mapped)
+    const fave = entries.reduce((max, current) =>
+    {
+        return (current[1] > max[1])
+        ? current
+        : max
+    })
+    console.log(
+        {
+            author: fave[0],
+            blogs: fave[1]
+        }
+    )
+    return {
+        author: fave[0],
+        blogs: fave[1]
+    }
+
+}
+
+const mostLikes = blogs =>
+{
+    const groups = lodash.groupBy(blogs, 'author')
+    const mapped = lodash.mapValues(groups, value =>
+    {
+        const result = value.reduce(( total, current ) =>
+        {
+            return total + current.likes
+        }, 0)
+        return result
+    })
+    const entries = Object.entries(mapped)
+    const fave = entries.reduce((max, current) =>
+    {
+        return (current[1] > max[1])
+        ? current
+        : max
+    })
+    console.log(
+        {
+            author: fave[0],
+            likes: fave[1]
+        }
+    )
+    return {
+        author: fave[0],
+        likes: fave[1]
+    }
+}
+
 module.exports = 
 {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    favouriteAuthor,
+    mostLikes
 }
