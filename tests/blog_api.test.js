5155b3b6fce552b85ba7e9c54dc0f621d9cc3119
Fyscher Kraal <fyscher.kraal@gmail.com>
2025-06-14 12:04:17 -0600

    sorting out describe blocks and tests to handle exercises better

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -16,100 +16,110 @@ beforeEach(async () =>
     await Blog.insertMany(helper.initialBlogs)
 })
 
-
-test('blogs are returned as json', async () =>
+describe ('Step 1: All blogs returned as JSON', () =>
 {
-    await api
+    test('all blogs are returned', async () =>
+    {
+        const res = await api.get('/api/blogs')
+        assert.strictEqual(res.body.length, helper.initialBlogs.length)
+    })
+
+    test('blogs are returned as json', async () =>
+    {
+        await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+    })
 })
 
-test('all blogs are returned', async () =>
-{
-    const res = await api.get('/api/blogs')
-    assert.strictEqual(res.body.length, helper.initialBlogs.length)
-})
+    // test('a specific blog is within the returned blogs', async () =>
+    // {
+    //     const res = await api.get('/api/blogs')
+    //     const titles = res.body.map(e => e.title)
+    //     assert.strictEqual(titles.includes('HTML is too Easy'), true)
+    // })
+        
+    // test('a specific blog can be viewed', async () =>
+    // {
+    //     const blogsAtStart = await helper.blogsInDb()
+    //     const blogToView = blogsAtStart[0]
+        
+    //     const resultBlog = await api
+    //     .get(`/api/blogs/${blogToView.id}`)
+    //     .expect(200)
+    //     .expect('Content-Type', /application\/json/)
+        
+    //     console.log('blogToView: ', blogToView)
+    //     console.log('resultBlog.body ', resultBlog.body)
+    //     assert.deepStrictEqual(resultBlog.body, blogToView)
+    // })
+                    
+// describe ('About Adding Blogs: Valid and Denying Invalid', () =>
+// {
+//     test('a valid blog can be added ', async () =>
+//     {
+//         const newBlog =
+//         {
+//             title: 'WE HERE TO TEST',
+//             author: 'Fyscher',
+//             url: 'www.com',
+//             likes: 123
+//         }
 
-test('a specific blog is within the returned blogs', async () =>
-{
-    const res = await api.get('/api/blogs')
-    const titles = res.body.map(e => e.title)
-    assert.strictEqual(titles.includes('HTML is too Easy'), true)
-})
+//         await api
+//             .post('/api/blogs')
+//             .send(newBlog)
+//             .expect(201)
+//             .expect('Content-Type', /application\/json/)
 
-test('a valid blog can be added ', async () =>
-{
-    const newBlog =
-    {
-        title: 'WE HERE TO TEST',
-        author: 'Fyscher',
-        url: 'www.com',
-        likes: 123
-    }
+//         const blogsAtEnd = await helper.blogsInDb()
+//         assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+//         const titles = blogsAtEnd.map(r => r.title)
+//         assert(titles.includes('WE HERE TO TEST'))
 
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+//     })
 
-    const titles = blogsAtEnd.map(r => r.title)
-    assert(titles.includes('WE HERE TO TEST'))
+//     test('a blog without a title cannot be added ', async () =>
+//     {
+//         const newBlog = 
+//         {
+//             author: 'Frank',
+//             url: 'www.com',
+//             likes: 0
+//         }
 
-})
+//         await api
+//             .post('/api/blogs')
+//             .send(newBlog)
+//             .expect(400)
 
-test('a blog without a title cannot be added ', async () =>
-{
-    const newBlog = 
-    {
-        author: 'Frank',
-        url: 'www.com',
-        likes: 0
-    }
+//         const blogsAtEnd = await helper.blogsInDb()
+//         console.log('blogs after blog rejected: ', blogsAtEnd)
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+//         assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+//     })
+// })
 
-    const blogsAtEnd = await helper.blogsInDb()
-    console.log('blogs after blog rejected: ', blogsAtEnd)
-
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-})
-
-test('a specific blog can be viewed', async () =>
-{
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToView = blogsAtStart[0]
-
-    const resultBlog = await api
-        .get(`/api/blogs/${blogToView.id}`)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+// describe ('About Removing Blogs: Can delete successfully', () =>
+// {
+//     test('a specific blog can be deleted', async () =>
+//     {
+//         const blogsAtStart = await helper.blogsInDb()
+//         const blogToDelete = blogsAtStart[0]
     
-    assert.deepStrictEqual(resultBlog.body, blogToView)
-})
-
-test('a specific blog can be deleted', async () =>
-{
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
-    await api
-        .delete(`/api/blogs/${blogToDelete.id}`)
-        .expect(204)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    const titles = blogsAtEnd.map(r => r.title)
+//         await api
+//             .delete(`/api/blogs/${blogToDelete.id}`)
+//             .expect(204)
     
-    assert(!titles.includes(blogToDelete.title))
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
-})
+//         const blogsAtEnd = await helper.blogsInDb()
+    
+//         const titles = blogsAtEnd.map(r => r.title)
+        
+//         assert(!titles.includes(blogToDelete.title))
+//         assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+//     })
+// })
 
 after(async () => await mongoose.connection.close())
